af907a18166d700e3f38845836e751788337a60d
Novelworm <mikhaya.jrh@gmail.com>
2021-09-27 12:26:19 +0700

    maps example

---
+++ 08_maps/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Define map --> make(map[key_type]value_type)
+	// emails := make(map[string]string)
+
+	// Assign key-values
+	// emails["Bob"] = "bob@gmail.com"
+	// emails["Sharon"] = "sharon@gmail.com"
+
+	// Declare map and add key-values
+	emails := map[string]string{
+		"Bob":    "bob@gmail.com",
+		"Sharon": "sharon@gmail.com",
+	}
+
+	emails["Mike"] = "mike@gmail.com"
+
+	fmt.Println(emails)
+	fmt.Println(len(emails))
+	fmt.Println(emails["Bob"])
+
+	// Delete from map
+	delete(emails, "Bob")
+
+	fmt.Println(emails)
+}
